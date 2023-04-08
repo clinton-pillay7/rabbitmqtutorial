2e5d6d3381b186950c54db458c37835a5800421c
clinton <clinton.pillay@outlook.com>
2023-04-08 21:16:40 +0200

    receive.py added

--- receive.py
+++ receive.py
@@ -1,21 +1,23 @@
+# import my modules
 import pika, sys, os
 
-def main():
-	credentials = pika.PlainCredentials('root', 'rootpassword')
-	connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.8.108',5672,'/',credentials))
-	channel = connection.channel()
-	channel.queue_declare(queue='hello')
+
+def main():                   																
+	credentials = pika.PlainCredentials('root', 'rootpassword')								# create variable for authentication
+	connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.8.108',5672,'/',credentials))			# create connection
+	channel = connection.channel()												# create new channel
+	channel.queue_declare(queue='hello')											# create new queue
 
 
-	def callback(ch, method, properties, body):
+	def callback(ch, method, properties, body):										# create function for displaying the message received
 		print(" [x] Received %r" % body)
 
-	channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+	channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)  					# setting parameters to receive the message 
 
-	print(' [*] Waiting for messages. To exit press CTRL+C')
+	print(' [*] Waiting for messages. To exit press CTRL+C')								# receiving the actual message
 	channel.start_consuming()
 
-if __name__ == '__main__':
+if __name__ == '__main__':													# flask code to run the app, and also to terminate the app. 
 	try:
 		main()
 	except KeyboardInterrupt:
