7f3474f7061a08b4b0af733568edb0d32faeb441
محسن عباسی <developer961@gmail.com>
2023-10-31 16:00:58 +0330

    Change arg 'on_message_callback' into 'consumer_callback'

--- receive.py
+++ receive.py
@@ -4,7 +4,7 @@ import pika, sys, os
 
 def main():                   																
 	credentials = pika.PlainCredentials('root', 'rootpassword')								# create variable for authentication
-	connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.8.108',5672,'/',credentials))			# create connection
+	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))			# create connection
 	channel = connection.channel()												# create new channel
 	channel.queue_declare(queue='hello')											# create new queue
 
@@ -12,7 +12,8 @@ def main():
 	def callback(ch, method, properties, body):										# create function for displaying the message received
 		print(" [x] Received %r" % body)
 
-	channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)  					# setting parameters to receive the message 
+#	channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)  					# setting parameters to receive the message 
+	channel.basic_consume(queue='hello', consumer_callback=callback)  					# setting parameters to receive the message 
 
 	print(' [*] Waiting for messages. To exit press CTRL+C')								# receiving the actual message
 	channel.start_consuming()
